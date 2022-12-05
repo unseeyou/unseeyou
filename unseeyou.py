e2ec41d1e5f49590133b585fb29c1dc30e893678
unseeyou <84846449+unseeyou@users.noreply.github.com>
2022-12-06 10:56:27 +1100

    Create unseeyou.py

---
+++ unseeyou.py
@@ -0,0 +1,10 @@
+def contact_me():
+    if unseeyou.status() != "busy":
+        return "Hello! I am available for contact."
+    else:
+        return "Unfortunately I am busy. Ping me on discord if urgent."
+
+      
+if __name__ == "__main__":
+    contact_me()
+    
